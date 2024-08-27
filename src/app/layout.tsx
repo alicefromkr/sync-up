00bd56fa98eb0c05a929563011004c5f9cbf7619
Alice <jaeyeon0921@gmail.com>
2024-08-27 21:04:19 +0900

    Project init

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { UIProvider } from "@/providers/uiprovider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sync Up",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
+      <body
+        className={`${inter.className} min-w-[320px] max-w-full overflow-x-hidden`}
+      >
+        <UIProvider>{children}</UIProvider>
+      </body>
+    </html>
+  );
+}
